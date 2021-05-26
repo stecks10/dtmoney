a4cf0951a7b20ff8e134d08ee1f8d0a6778fd4e1
Vitor Nunes <stecks10@gmail.com>
2021-05-26 16:36:57 -0300

    Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
-  return (
-    <div className="App">
-      <h1 >
-        Hello World
-      </h1>
-      <GlobalStyle />
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <GlobalStyle />
+        </>
+    );
 }
 
