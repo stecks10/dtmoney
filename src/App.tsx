12d76b81300a435a595208a71831dad350d587f1
Vitor Nunes <stecks10@gmail.com>
2021-05-27 17:36:39 -0300

    carregando transactions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { GlobalStyle } from "./styles/global";
 
@@ -10,6 +11,8 @@ Modal.setAppElement('#root');
 
 export function App() {
    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+
 
    function handleOpenNewTransactionModal() {
       setIsNewTransactionModalOpen(true);
@@ -20,7 +23,7 @@ export function App() {
    }
 
    return (
-      <>
+      <TransactionsProvider>
          <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
          <Dashboard />
@@ -31,7 +34,7 @@ export function App() {
          />
 
          <GlobalStyle />
-      </>
+      </TransactionsProvider>
    );
 }
 
